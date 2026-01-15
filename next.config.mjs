0f82b030c418c0067a5f907c1fa1450683c75f83
Ch.k.Rishi <chkrishi4@gmail.com>
2026-01-15 11:31:15 +0530

    not so final

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    turbopack: {}
+    eslint: {
+        ignoreDuringBuilds: true
+    }
 };
 
 export default nextConfig;
